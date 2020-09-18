ac132f923099323c0a962f847ad9eedca41be57b
Joey <josephdguillaume@gmail.com>
2020-09-18 14:34:31 -0400

    Completed Exercise 0.5

---
+++ 0.getting-started/exercise0.5.js
@@ -0,0 +1,4 @@
+// .substring() is a prototype method available on the String constructor.
+// .create() is a static method available on the Object constructor.
+// .fromCharCode() is a static method available on the String constructor.
+// .slice() is a prototype method available on the Array and String constructors.
