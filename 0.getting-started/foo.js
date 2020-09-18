c125b06395f1c5af8c5eb3a7da3006f68447356c
Joey <josephdguillaume@gmail.com>
2020-09-18 13:52:57 -0400

    Completed Exercise 0.4

--- 0.getting-started/foo.js
+++ 0.getting-started/foo.js
@@ -1,3 +1,7 @@
 const foo = 'bar';
 console.log(foo);
 foo;
+
+// Running the command `node foo.js` returned "bar".
+// Pasting into Node returned undefined for the first two lines of code and "bar" for the last.
+// Pasting the code into Chrome returned "bar".
