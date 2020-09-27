c77b24c1cf349a86ccaaaa68ad720fc55c599cc6
Joey <josephdguillaume@gmail.com>
2020-09-27 13:34:47 -0400

    Completed Exercise 1.5

---
+++ 1.the-basics/exercise1.5.js
@@ -0,0 +1,5 @@
+// Explain why this code logs '510' instead of 15.
+// console.log('5' + 10);
+
+/* The code logs '510' due to implicit type coercion. When a string value is involved in an operation with a non-string value,
+   JavaScript will attempt to coerce the non-string value to a String type. */
