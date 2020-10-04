ad3795f5e402d2dcdda0e9ca34cad41089f74988
Joey <josephdguillaume@gmail.com>
2020-10-04 16:05:56 -0400

    Completed Exercise 7.4

---
+++ 7.arrays/exercise7.4.js
@@ -0,0 +1,11 @@
+const myArray = [1, 3, 6, 11, 4, 2, 4, 9, 17, 16, 0];
+
+function classifyEvenOrOdd(number) {
+  if (number % 2 === 0) {
+    return 'even';
+  } else {
+    return 'odd';
+  }
+}
+
+console.log(myArray.map(classifyEvenOrOdd));
