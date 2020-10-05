70346f2132087e8dba793f40a43d20793d11ea80
Joey <josephdguillaume@gmail.com>
2020-10-05 00:22:31 -0400

    Completed Exercise 8.8

---
+++ 8.objects/exercise8.8.js
@@ -0,0 +1,2 @@
+// The first console.log() statement outputs 'hi'. The bar function mutates the original object, changing its a property to the 'hi' value.
+// The second console.log() statement outputs 'hello'. The bar function can't mutate a primitive, and thus the value of qux is still 'hello'.
