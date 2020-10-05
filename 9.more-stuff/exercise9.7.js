eacfbbb2c7f09e6730bcddead494dbdf7d1b5014
Joey <josephdguillaume@gmail.com>
2020-10-05 10:55:33 -0400

    Completed Exercise 9.7

---
+++ 9.more-stuff/exercise9.7.js
@@ -0,0 +1,2 @@
+// Exception handling is the process of managing errors in JavaScript in a predictable manner.
+// In JavaScript, we use the try/catch statement to tell JavaScript what to do if a specific error is encountered.
